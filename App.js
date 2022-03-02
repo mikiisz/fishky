ca75f6ba544ddaabcb359e1ce36d8fa4ae63f7e4
mszkarlat <michal.szkarlat@grandparade.co.uk>
2022-03-02 23:16:27 +0100

    use template

--- App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {registerRootComponent} from "expo"
-import MainStackNavigator from "./src/navigation/MainStackNavigator"
-
-function App() {
-    return <MainStackNavigator/>
-}
-
-registerRootComponent(App)
